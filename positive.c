32d799c4ac67951927f3974933c4845eec4e5055
venu-0717 <venunaidukaranam2006@gmail.com>
2026-04-22 16:20:32 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a;
+    printf("Enter the A value \n");
+    scanf("%d",&a);
+    printf("A is a positive i.e is:%d",a>0);
+    return 0;
+}
